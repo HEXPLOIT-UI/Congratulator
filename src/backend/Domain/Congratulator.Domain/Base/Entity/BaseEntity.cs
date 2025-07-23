aa2be70335bb753ff9f32d30d803497d3bceda9c
HEXPLOIT-UI <20075099-HEXPLOIT-UI@users.noreply.gitlab.com>
2025-07-23 19:30:56 +0300

    Add project files.

---
+++ src/backend/Domain/Congratulator.Domain/Base/Entity/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace Congratulator.Domain.Base.Entity;
+
+/// <summary>
+/// Базовый класс для всех сущностей.
+/// </summary>
+public class BaseEntity
+{
+    /// <summary>
+    /// Идентификатор записи.
+    /// </summary>
+    public Guid EntityId { get; set; }
+
+    /// <summary>
+    /// Дата создания записи.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+}
